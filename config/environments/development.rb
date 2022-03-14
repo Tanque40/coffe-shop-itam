4ccfefec2f9d060273b4f220354e22d5a34b2947
Bruno Vitte <bruno_vitte@hotmail.com>
2022-03-14 13:31:34 -0600

    Inicializando simple_form, incializar usuarios devise, configurar devise básico

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  #################################
+  #       Personal configs        #
+  #################################
+  # In production, :host should be set to the actual host of your application. 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # This is for devise gem service
 end
