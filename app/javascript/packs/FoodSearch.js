f395b1c3294158c6c32e1ef237e81d4a983f5e16
Bruno Vitte <bruno_vitte@hotmail.com>
2022-04-24 19:13:18 -0500

    add js interaction in forms

--- app/javascript/packs/FoodSearch.js
+++ app/javascript/packs/FoodSearch.js
@@ -1,11 +1,43 @@
 export class FoodSearch{
-    constructor(){
-    }    
-
-    test(){
     
-	console.log("hello FoodSearch");
+    constructor(){
+	
+	// Key for spoonacular API
+	this.API_KEY = "apiKey=91a321fa14be4eee964e136880ae081c";
+	//API Link
+	this.link = "https://api.spoonacular.com/";
+    
+    }    
+    
+    // Start a search on spoonacular API and 
+    // show images on .photosContainer 
+    searchOptions(foodName){
+	
+	const linkSearch = `${this.link}recipes/complexSearch?${this.API_KEY}&query=${foodName}`;
+	console.log(linkSearch);
+	$.get(linkSearch, function(data){
+	    console.table(data);
+        
+        var message = `<div class="row">`;
+	    var countImage = 1;
+	    data.results.forEach(element => {
+	    
+	        message += `<div class="foodPhoto col">
+	                        <a href="javascript:void(0)" class="foodPhotoLink" id="${element.id}">
+	                            <img src="${element.image}" alt="foodName option ${countImage}" class="img-fluid img-thumbail">
+	                        </a>    
+                       </div>`;
+	        countImage++;
+	    
+	    });
+        message += `</div>`
+
+	    $('div.foodPhotosContainer').html(
+            message
+	    );
+	});
 
     }
 
+    
 }
--- app/javascript/packs/food.js
+++ app/javascript/packs/food.js
@@ -2,12 +2,18 @@ import $ from 'jquery'
 
 import { FoodSearch } from './FoodSearch'
 
-/*HOLA*/
-$(
-    function(){
-    
-	var f = new FoodSearch();
-	f.test();
+const f = new FoodSearch();
 
-    }
-);
+$(document).ready(function(){
+	
+    $("#food_photo").keyup(function() {
+    
+	f.searchOptions(
+	    $("#food_photo").val()
+	);
+
+    });
+
+});
+
+
