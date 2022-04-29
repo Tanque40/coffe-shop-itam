0d6f1128ed1edb20ad258dda9233afb59eec2cf8
Bruno Vitte <bruno_vitte@hotmail.com>
2022-04-29 13:39:52 -0500

    añadiendo estilos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("channels")
 import 'bootstrap'
 import 'roboto-fontface'
 
-import '../src/stylesheets/application.sass'
+import '../src/stylesheets/application'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
